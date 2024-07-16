58fe12709a1707f213fc7539766ac059217eae4e
cristianOvando <ovandogomez60@gmail.com>
2024-07-15 23:04:59 -0600

    Front

--- src/components/molecules/login.jsx
+++ src/components/molecules/login.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserContext from '../../Context/UserContext';
-import '../../assets/css/login.css';
+import '../../assets/css/login.css';  // Verifica que el path sea correcto
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const {isLoged, setIsLoged} = useContext(UserContext);
+  const { setIsLoged } = useContext(UserContext);
   const navigate = useNavigate();
 
   const users = [
@@ -16,9 +16,7 @@ function Login() {
   ];
 
   const handleLogin = () => {
-    const foundUser = users.find(
-      (user) => user.username === username && user.password === password
-    );
+    const foundUser = users.find(user => user.username === username && user.password === password);
     if (foundUser) {
       setIsLoged(true);
       navigate("/home");
@@ -28,37 +26,35 @@ function Login() {
   };
 
   return (
-    <>
-      <div className="login-container">
-        <div className="login-card">
-          <div className="login-logo"></div>
-          <div className="input-group">
-            <input
-              className="input"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className="input"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="remember-me">
-            <input type="checkbox" id="rememberMe" />
-            <label htmlFor="rememberMe">Remember me</label>
-          </div>
-          <button className="login-btn" onClick={handleLogin}>
-            Login
-          </button>
-          <p className="forgot-password">Forgot password?</p>
+    <div className="login-container">
+      <div className="login-card">
+        <div className="login-logo"></div>
+        <div className="input-group">
+          <input
+            className="input"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+        <div className="remember-me">
+          <input type="checkbox" id="rememberMe" />
+          <label htmlFor="rememberMe">Remember me</label>
+        </div>
+        <button className="login-btn" onClick={handleLogin}>
+          Login
+        </button>
+        <p className="forgot-password">Forgot password?</p>
       </div>
-    </>
+    </div>
   );
 }
 
